668ab8582ebc3108846c69b87645ed3a62752cb7
BBehr960 <behr.30@osu.edu>
2022-03-28 23:50:13 -0400

    Added method bodies for addItem subclasses

--- src/Adder.java
+++ src/Adder.java
@@ -52,21 +52,21 @@ public class Adder {
             case "album":
                 addAlbum(item, conn, scan, newItemID);
                 break;
-            // case "track":
-            // addTrack(item, conn, scan, newItemID);
-            // break;
-            // case "interview":
-            // addInterview(item, conn, scan, newItemID);
-            // break;
-            // case "movie":
-            // addMovie(item, conn, scan, newItemID);
-            // break;
-            // case "audiobook":
-            // addAudiobook(item, conn, scan, newItemID);
-            // break;
-            // case "physicalbook":
-            // addPhysicalbook(item, conn, scan, newItemID);
-            // break;
+            case "track":
+                addTrack(item, conn, scan, newItemID);
+                break;
+            case "interview":
+                addInterview(item, conn, scan, newItemID);
+                break;
+            case "movie":
+                addMovie(item, conn, scan, newItemID);
+                break;
+            case "audiobook":
+                addAudiobook(item, conn, scan, newItemID);
+                break;
+            case "physicalbook":
+                addPhysicalbook(item, conn, scan, newItemID);
+                break;
             default:
                 System.err.println(item + " isn't a valid new item insert type");
         }
@@ -99,140 +99,134 @@ public class Adder {
         }
     }
 
-    // private static void addTrack(String item, Connection conn, Scanner scan, int
-    // newItemID) {
-    // PreparedStatement stmt = null;
+    private static void addTrack(String item, Connection conn, Scanner scan, int newItemID) throws Exception {
+        PreparedStatement stmt = null;
 
-    // try {
-    // System.out.println("Please enter the number of songs");
-    // int numSongs = Integer.valueOf(scan.nextLine());
+        try {
+            System.out.println("Please enter the AlbumID corresponding to this track");
+            int albumID = Integer.valueOf(scan.nextLine());
 
-    // System.out.println("Please enter the length in minutes");
-    // int lenInMin = Integer.valueOf(scan.nextLine());
+            System.out.println("Please enter the length in seconds");
+            int lenInSec = Integer.valueOf(scan.nextLine());
 
-    // String query = "insert into album values (?,?,?);";
-    // stmt = conn.prepareStatement(query);
+            String query = "insert into track values (?,?,?);";
+            stmt = conn.prepareStatement(query);
 
-    // stmt.setInt(1, numSongs);
-    // stmt.setInt(2, lenInMin);
-    // stmt.setInt(3, newItemID);
+            stmt.setInt(1, lenInSec);
+            stmt.setInt(2, albumID);
+            stmt.setInt(3, newItemID);
 
-    // stmt.executeUpdate();
-    // } catch (SQLException | NumberFormatException e) {
-    // System.out.println(e.getMessage());
-    // throw e;
-    // } finally {
-    // Util.closeStmt(stmt);
-    // }
-    // }
+            stmt.executeUpdate();
+        } catch (SQLException | NumberFormatException e) {
+            System.out.println(e.getMessage());
+            throw e;
+        } finally {
+            Util.closeStmt(stmt);
+        }
+    }
 
-    // private static void addInterview(String item, Connection conn, Scanner scan,
-    // int newItemID) {
-    // PreparedStatement stmt = null;
+    private static void addInterview(String item, Connection conn, Scanner scan,
+            int newItemID) throws Exception {
+        PreparedStatement stmt = null;
 
-    // try {
-    // System.out.println("Please enter the number of songs");
-    // int numSongs = Integer.valueOf(scan.nextLine());
+        try {
 
-    // System.out.println("Please enter the length in minutes");
-    // int lenInMin = Integer.valueOf(scan.nextLine());
+            System.out.println("Please enter the length in minutes");
+            int lenInMin = Integer.valueOf(scan.nextLine());
 
-    // String query = "insert into album values (?,?,?);";
-    // stmt = conn.prepareStatement(query);
+            String query = "insert into album values (?,?);";
+            stmt = conn.prepareStatement(query);
 
-    // stmt.setInt(1, numSongs);
-    // stmt.setInt(2, lenInMin);
-    // stmt.setInt(3, newItemID);
+            stmt.setInt(1, lenInMin);
+            stmt.setInt(2, newItemID);
 
-    // stmt.executeUpdate();
-    // } catch (SQLException | NumberFormatException e) {
-    // System.out.println(e.getMessage());
-    // throw e;
-    // } finally {
-    // Util.closeStmt(stmt);
-    // }
-    // }
+            stmt.executeUpdate();
+        } catch (SQLException | NumberFormatException e) {
+            System.out.println(e.getMessage());
+            throw e;
+        } finally {
+            Util.closeStmt(stmt);
+        }
+    }
 
-    // private static void addMovie(String item, Connection conn, Scanner scan, int
-    // newItemID) {
-    // PreparedStatement stmt = null;
+    private static void addMovie(String item, Connection conn, Scanner scan, int newItemID) throws Exception {
+        PreparedStatement stmt = null;
 
-    // try {
-    // System.out.println("Please enter the number of songs");
-    // int numSongs = Integer.valueOf(scan.nextLine());
+        try {
+            System.out.println("Please enter the runtime in minutes");
+            int runtime = Integer.valueOf(scan.nextLine());
 
-    // System.out.println("Please enter the length in minutes");
-    // int lenInMin = Integer.valueOf(scan.nextLine());
+            System.out.println("Please enter the rating");
+            String rating = scan.nextLine();
 
-    // String query = "insert into album values (?,?,?);";
-    // stmt = conn.prepareStatement(query);
+            String query = "insert into album values (?,?,?);";
+            stmt = conn.prepareStatement(query);
 
-    // stmt.setInt(1, numSongs);
-    // stmt.setInt(2, lenInMin);
-    // stmt.setInt(3, newItemID);
+            stmt.setInt(1, runtime);
+            stmt.setString(2, rating);
+            stmt.setInt(3, newItemID);
 
-    // stmt.executeUpdate();
-    // } catch (SQLException | NumberFormatException e) {
-    // System.out.println(e.getMessage());
-    // throw e;
-    // } finally {
-    // Util.closeStmt(stmt);
-    // }
-    // }
+            stmt.executeUpdate();
+        } catch (SQLException | NumberFormatException e) {
+            System.out.println(e.getMessage());
+            throw e;
+        } finally {
+            Util.closeStmt(stmt);
+        }
+    }
 
-    // private static void addAudiobook(String item, Connection conn, Scanner scan,
-    // int newItemID) {
-    // PreparedStatement stmt = null;
+    private static void addAudiobook(String item, Connection conn, Scanner scan,
+            int newItemID) throws Exception {
+        PreparedStatement stmt = null;
 
-    // try {
-    // System.out.println("Please enter the number of songs");
-    // int numSongs = Integer.valueOf(scan.nextLine());
+        try {
+            System.out.println("Please enter the number of chapters");
+            int numChapters = Integer.valueOf(scan.nextLine());
 
-    // System.out.println("Please enter the length in minutes");
-    // int lenInMin = Integer.valueOf(scan.nextLine());
+            System.out.println("Please enter the length in minutes");
+            int lenInMin = Integer.valueOf(scan.nextLine());
 
-    // String query = "insert into album values (?,?,?);";
-    // stmt = conn.prepareStatement(query);
+            String query = "insert into album values (?,?,?);";
+            stmt = conn.prepareStatement(query);
 
-    // stmt.setInt(1, numSongs);
-    // stmt.setInt(2, lenInMin);
-    // stmt.setInt(3, newItemID);
+            stmt.setInt(1, numChapters);
+            stmt.setInt(2, lenInMin);
+            stmt.setInt(3, newItemID);
 
-    // stmt.executeUpdate();
-    // } catch (SQLException | NumberFormatException e) {
-    // System.out.println(e.getMessage());
-    // throw e;
-    // } finally {
-    // Util.closeStmt(stmt);
-    // }
-    // }
+            stmt.executeUpdate();
+        } catch (SQLException | NumberFormatException e) {
+            System.out.println(e.getMessage());
+            throw e;
+        } finally {
+            Util.closeStmt(stmt);
+        }
+    }
 
-    // private static void addPhysicalbook(String item, Connection conn, Scanner
-    // scan, int newItemID) {
-    // PreparedStatement stmt = null;
+    private static void addPhysicalbook(String item, Connection conn, Scanner scan, int newItemID) throws Exception {
+        PreparedStatement stmt = null;
 
-    // try {
-    // System.out.println("Please enter the number of songs");
-    // int numSongs = Integer.valueOf(scan.nextLine());
+        try {
+            System.out.println("Please enter the number of chapters");
+            int numChapters = Integer.valueOf(scan.nextLine());
 
-    // System.out.println("Please enter the length in minutes");
-    // int lenInMin = Integer.valueOf(scan.nextLine());
+            System.out.println("Please enter the number of pages");
+            int pages = Integer.valueOf(scan.nextLine());
 
-    // String query = "insert into album values (?,?,?);";
-    // stmt = conn.prepareStatement(query);
+            String query = "insert into album values (?,?,?);";
+            stmt = conn.prepareStatement(query);
 
-    // stmt.setInt(1, numSongs);
-    // stmt.setInt(2, lenInMin);
-    // stmt.setInt(3, newItemID);
+            stmt.setInt(1, numChapters);
+            stmt.setInt(2, pages);
+            stmt.setInt(3, newItemID);
 
-    // stmt.executeUpdate();
-    // } catch (SQLException | NumberFormatException e) {
-    // System.out.println(e.getMessage());
-    // throw e;
-    // } finally {
-    // Util.closeStmt(stmt);
-    // }
-    // }
+            stmt.executeUpdate();
+        } catch (SQLException | NumberFormatException e) {
+            System.out.println(e.getMessage());
+            throw e;
+        } finally {
+            Util.closeStmt(stmt);
+        }
+    }
 
     // public static void addItemGenre(String item, Connection conn, Scanner scan,
     // int newItemID) {
