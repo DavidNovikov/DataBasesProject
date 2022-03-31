55578dd69966b81446d0d216e86b376953653e19
ButtonBill <Willakarra@gmail.com>
2022-03-31 19:18:15 -0400

    Fixed formatting

--- src/Checkout.java
+++ src/Checkout.java
@@ -1,4 +1,5 @@
 import java.sql.*;
+import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.util.*;
 import java.util.Date;
@@ -8,21 +9,24 @@ public class Checkout {
 		PreparedStatement stmt = null;
 		int itemID = Searcher.pickItem(type, conn, scan);
 		int cardID = Searcher.pickPerson(conn, scan);
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
 		java.sql.Date currentDate = new java.sql.Date(new java.util.Date().getTime());
 		LocalDate ld = currentDate.toLocalDate();
 		LocalDate threeWeeksLater = ld.plusWeeks(3);
 		java.sql.Date dueDate = java.sql.Date.valueOf(threeWeeksLater);
+		String formattedCurrentDate = sdf.format(currentDate);
+		String formattedDueDate = sdf.format(dueDate);
 		
 		try {
 			String sql = "insert into item_checked_out values (?,?,?,?,?);";
 			stmt = conn.prepareStatement(sql);
 			stmt.setInt(1, itemID);
 			stmt.setInt(2, cardID);
-			stmt.setDate(3, (java.sql.Date) currentDate);
+			stmt.setString(3, formattedCurrentDate);
 			//Temporary check if this gives current date or not
 			stmt.setNull(4, Types.NULL);
 			//Temporary check if this gives current date or not
-			stmt.setDate(5, (java.sql.Date) dueDate);
+			stmt.setString(5, formattedDueDate);
 			stmt.executeUpdate();
             
 			
